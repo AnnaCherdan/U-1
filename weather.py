bc3884ba43dc7bd3ad3cf0b18f426515c454329f
AnnaCherdan <anacher@mail.ru>
2022-03-30 00:55:57 +0700

    Added language

--- weather.py
+++ weather.py
@@ -3,18 +3,23 @@ import pyowm
 from pyowm import OWM
 from pyowm.utils import config
 from pyowm.utils import timestamps
-owm = OWM('3ef0b621ba1d0ad74912abfb7b925184')# , language = "ru"
+from pyowm.utils.config import get_default_config
+
+config_dict = get_default_config()
+config_dict['lang'] = 'ru'
+
+owm = OWM('3ef0b621ba1d0ad74912abfb7b925184', config_dict)
 mgr = owm.weather_manager()
 
-place = input("Where are you from?: ")
+place = input("Какой город вас интересует?: ")
 
 observation = mgr.weather_at_place('place')
 w = observation.weather
 
 temp = w.temperature('celsius')["temp"]
 
-print("In the city " + place + " now is " + w.detailed_status)
-print("The temperature in the city now approximately: " + str(temp))
+print("В интересном вам городе " + place + " сейчас " + w.detailed_status)
+print("Приблизительная температура: " + str(temp))
 
 if temp < 10:
     print("Прохладно, одевайся тепло")
